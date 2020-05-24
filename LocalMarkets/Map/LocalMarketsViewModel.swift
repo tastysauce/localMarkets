b483215eedb69adc1213434a4628cecb32f62f6f
Michael Koch <tastysauce@gmail.com>
2020-05-23 17:07:00 -0700

    Getting ready to show annotations

--- LocalMarkets/Map/LocalMarketsViewModel.swift
+++ LocalMarkets/Map/LocalMarketsViewModel.swift
@@ -11,7 +11,7 @@ import Combine
 
 class LocalMarketsViewModel: ObservableObject {
 
-    @Published var markets: [NearbyMarket] = []
+    @Published var markets: [Market] = []
 
     private let marketsAPIClient: MarketsAPIClient
     private var disposeBag: Set<AnyCancellable> = []
@@ -25,9 +25,23 @@ class LocalMarketsViewModel: ObservableObject {
             .subscribe(on: DispatchQueue.global(qos: .background))
             .receive(on: DispatchQueue.main)
             .map(\.markets)
+            .flatMap { self.getDetailsForAll(nearbyMarkets: $0) }
             .replaceError(with: [])
             .assign(to: \.markets, on: self)
             .store(in: &disposeBag)
+    }
+
+    private func getDetailsForAll(nearbyMarkets: [NearbyMarket]) -> AnyPublisher<[Market], Error> {
+        let arrayOfPublishers = nearbyMarkets.map { getDetailsFor(nearbyMarket: $0) }
+        let sequenceOfPublishers = Publishers.Sequence<[AnyPublisher<Market, Error>], Error>(sequence: arrayOfPublishers)
+        let marketsPublisher = sequenceOfPublishers.flatMap { $0 }.collect().eraseToAnyPublisher()
+        return marketsPublisher
+    }
+
+    private func getDetailsFor(nearbyMarket: NearbyMarket) -> AnyPublisher<Market, Error> {
+        marketsAPIClient.requestMarketDetails(for: nearbyMarket)
+            .map { Market(nearbyMarket: nearbyMarket, nearbyMarketDetails: $0.marketDetails) }
+            .eraseToAnyPublisher()
     }
 
     static var mockLocalMarkets: LocalMarketsViewModel {
